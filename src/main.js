2c33f548273088d5031e7455e1158059e78e6f4a
Eliott Collin <eliott.collin.etu@univ-lille.fr>
2022-03-04 20:40:12 +0100

    simple index.html and main.js added

---
+++ src/main.js
@@ -0,0 +1,5 @@
+document.querySelector(".video").innerHTML = ` <video controls width="250">
+                                                            <source src="https://i.imgur.com/MxAE8Wp.mp4"
+                                                                type="video/mp4">
+                                                            hehehe (pardon)
+                                                        </video>;`

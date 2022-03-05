26ab37b4f3b6636cda1d3e3541a694210711a5a3
Eliott Collin <eliott.collin.etu@univ-lille.fr>
2022-03-05 20:07:09 +0100

    use Router in main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,22 @@
-document.querySelector(".video").innerHTML = ` <video controls width="250">
-                                                            <source src="https://i.imgur.com/MxAE8Wp.mp4"
-                                                                type="video/mp4">
-                                                            hehehe (pardon)
-                                                        </video>;`
+import {Router} from "./Router";
+
+let homePage = "we trying to build the page";
+let FavPage = "No fav for now, come back later";
+let TeamPage = "The best you never see !";
+
+Router.titleElement = document.querySelector(".pageTitle");
+Router.contentElement = document.querySelector(".pageContent");
+Router.routes = [
+    {path: '/', page: homePage, title: "Work in progress ..."},
+    {path: '/favoris', page: FavPage, title: "Series you like, I think..."},
+    {path: '/equipe', page: TeamPage, title: "Our team"}
+];
+
+Router.menuElement = document.querySelector('.menu');
+
+Router.navigate(document.location.pathname);
+
+
+window.onpopstate = () => {
+    Router.navigate(document.location.pathname, false);
+};

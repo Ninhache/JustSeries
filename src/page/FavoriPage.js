aabaa656abe33448ddba197b5e820815576f3a6c
Eliott Collin <eliott.collin.etu@univ-lille.fr>
2022-03-08 20:51:30 +0100

    get serie with favs id and put it in the page

--- src/page/FavoriPage.js
+++ src/page/FavoriPage.js
@@ -1,12 +1,27 @@
 import {Page} from "./Page";
+import TvMazeRequester from "../api/TvMazeRequester";
 
 export class FavoriPage extends Page{
 
+    mount(element) {
+        super.mount(element);
 
+        const local = localStorage.getItem("favs_id");
+        const myFav = JSON.parse(local);
+
+        const request = new TvMazeRequester();
+
+        myFav.forEach( id => {
+            request.getById(id)
+                .then( data => data.json())
+                .then( data => {
+                    element.innerHTML += `- ${data.name} | ${id} <br>`; // todo : new FavCard().render()
+                })
+                .then(() => element.querySelector(".wait").hidden = true);
+        });
+    }
 
     render() {
-        const myFav = localStorage.getItem("favs_id");
-
-        return "we trying to built it";
+        return "<span class='wait'>Wait a moment plz </span>";
     }
 }

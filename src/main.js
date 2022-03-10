4132b46d5afb6ea08c79d5b1cfc9b49399b04f86
Néo Almeida <neo.almeida.etu@univ-lille.fr>
2022-03-10 01:05:14 +0100

    Fix merges errors

--- src/main.js
+++ src/main.js
@@ -1,23 +1,28 @@
-import {Router} from "./Router";
-import {HomePage} from "./page/HomePage";
-import {FavoriPage} from "./page/FavoriPage";
-import {TeamPage} from "./page/TeamPage";
+import { Router } from "./Router";
+import { HomePage } from "./page/HomePage";
+import { FavoriPage } from "./page/FavoriPage";
+import { TeamPage } from "./page/TeamPage";
 import { ResultPage } from "./pages/ResultPage";
+
+import PrototypeSearchBar from './PrototypeSearchBar';
+import TvMazeRequester from "./api/TvMazeRequester";
+import PrototypeCard from "./PrototypeCard";
 
 const homePage = new HomePage();
 const favPage = new FavoriPage();
 const teamPage = new TeamPage();
 const Results = new ResultPage();
 
+
 Router.menuElement = document.querySelector('.menu');
-Router.titleElement = document.querySelector(".pageTitle");
-Router.contentElement = document.querySelector(".pageContent");
+Router.titleElement = document.querySelector(".root > header > #title");
+Router.contentElement = document.querySelector(".root > #content");
 
 Router.routes = [
 	{path: '/', page: homePage, title: "Work in progress ..."},
 	{path: '/favoris', page: favPage, title: "Series you like, I think..."},
 	{path: '/equipe', page: teamPage, title: "Our team"},
-	{ path: '/search', page: Results, title: "TestTitre" }
+	{path: '/search', page: Results, title: "TestTitre" }
 ];
 
 Router.navigate(document.location.pathname);

f2be05d2c53072a0b85fb1b66aacedbb8d2e5985
Eliott Collin <eliott.collin0307@gmail.com>
2022-03-14 14:48:09 +0100

    get 16 random item on a random page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,22 +1,29 @@
 import {Page} from "./Page";
 import TvMazeRequester from "../api/TvMazeRequester";
-import Card from "../Card";
 import PrototypeCard from "../PrototypeCard";
 
 export class HomePage extends Page {
 
+    randomize(tab) {
+        return tab
+            .map(value => ({ value, sort: Math.random() }))
+            .sort((a, b) => a.sort - b.sort)
+            .map(({ value }) => value);
+    }
+
     mount(element) {
         super.mount(element);
+        let randomPage = Math.floor(Math.random() * 244)+1;
 
         const request = new TvMazeRequester();
-        request.getPage(0)
+        request.getPage(randomPage)
             .then(data => data.json())
+            .then(data => this.randomize(data))
             .then(data => {
                 element.innerHTML = ""
-                data.forEach(element => {
-                    this.element.appendChild(new PrototypeCard(element).render());
-                });
-
+                for (let i = 0; i < 16; i++) {
+                    this.element.appendChild(new PrototypeCard(data[i]).render());
+                }
             });
 
     }

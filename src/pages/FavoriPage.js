fce32b5360c6779a6a443b8920e15e3ec3155119
Néo Almeida <neo.almeida.etu@univ-lille.fr>
2022-03-14 17:24:45 +0100

    Trying to fix the old methods (was dirty..)

--- src/pages/FavoriPage.js
+++ src/pages/FavoriPage.js
@@ -2,8 +2,13 @@ import {Page} from "./Page";
 import TvMazeRequester from "../api/TvMazeRequester";
 import Card from "../Card";
 import PrototypeCard from "../PrototypeCard";
+import { favIsEmpty } from "../FavoriteHandler";
 
 export class FavoriPage extends Page{
+
+    render() {
+        return "<span class='wait'>Wait a moment plz </span>";
+    }
 
     mount(element) {
         super.mount(element);
@@ -12,16 +17,59 @@ export class FavoriPage extends Page{
         const myFav = JSON.parse(local);
 
         const request = new TvMazeRequester();
+        let index = 0;
+        const lengthMax = myFav.length -1;
+        
+        const promiseArray = [];
 
-        myFav.forEach( id => {
-            request.getById(id)
+        if (myFav.length > 0) {
+
+
+            myFav.forEach(id => promiseArray.push(request.getById(id).then(response => response.json())));
+            
+            Promise.allSettled(promiseArray).then((values) => {
+                console.log(values)
+                // faut ajouter les elements
+                /*
+                values.forEach(item => {
+                    console.log(item)
+                    element.appendChild(new PrototypeCard(item).render());
+                });
+                element.querySelector(".wait").classList.remove("wait");
+                */
+            });
+            
+
+            //promiseArray.push()
+            
+            /*
+            myFav.forEach( id => {
+                request.getById(id)
+                    .then( data => data.json())
+                    .then( data => {
+                        index++;
+                        element.appendChild(new PrototypeCard(data).render());    
+                    })
+
+                element.querySelector(".wait").classList.remove("wait");
+            });*/
+            
+
+            /*
                 .then( data => data.json())
-                .then( data => element.appendChild(new PrototypeCard(data).render()))
-                .then( () => element.querySelector(".wait").hidden = true);
-        });
+                .then( data => {
+                    index++;
+                    element.appendChild(new PrototypeCard(data).render());
+                })
+            */
+
+            //element.querySelector(".wait").classList.remove("wait");
+        } else {
+            element.innerHTML = "<h3>Pas de favoris ...</h3>"
+        }
+        
+        
+
     }
 
-    render() {
-        return "<span class='wait'>Wait a moment plz </span>";
-    }
 }

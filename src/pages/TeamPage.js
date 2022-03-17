0fe2662594cba683efe9052b08f03d5fa9e360dc
Eliott Collin <eliott.collin.etu@univ-lille.fr>
2022-03-17 20:32:15 +0100

    carousel display for team card is full functional !!! 😃😍

--- src/pages/TeamPage.js
+++ src/pages/TeamPage.js
@@ -5,17 +5,44 @@ import team from "../team.json"
 
 export class TeamPage extends Page{
 
+    selectedIndex = 0;
+
+    rotateCarousel() {
+        const angle = this.selectedIndex / 3 * -360;
+        this.element.querySelector(".team_carousel").style.transform = 'translateZ(-288px) rotateY(' + angle + 'deg)';
+    }
+
     mount(element) {
-        const teamContainer = element.querySelector(".team");
+        super.mount(element);
+        const teamContainer = element.querySelector(".team_carousel");
         teamContainer.innerHTML = "";
 
         team.forEach( teammate => {
              teamContainer.appendChild(new TeammateCard(teammate).render());
         });
+
+        const turnLeft = element.querySelector(".turnLeft");
+        turnLeft.addEventListener( "click", event => {
+            event.preventDefault();
+            this.selectedIndex--;
+            this.rotateCarousel();
+        })
+
+        const turnRight = element.querySelector(".turnRight");
+        turnRight.addEventListener( "click", event => {
+            event.preventDefault();
+            this.selectedIndex++;
+            this.rotateCarousel();
+        })
+
     }
 
     render() {
-        return `<div class="team">Nice team here ! ... they coming soon</div>`;
+        return `<div class="team_scene">
+                    <div class="team_carousel">Nice team here ! ... they coming soon</div>
+                    <button class="turnLeft"><i class="fa-solid fa-angle-left"></i></button>
+                    <button class="turnRight"><i class="fa-solid fa-angle-right"></i></button>
+                </div>`;
     }
 
 }

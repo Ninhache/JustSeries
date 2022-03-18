676fb5cda0bea8789f754160654569dc40993e06
Eliott Collin <eliott.collin.etu@univ-lille.fr>
2022-03-18 17:43:34 +0100

    Sort is available on favs page + fetch only new elements and keep those already done

--- src/pages/FavoriPage.js
+++ src/pages/FavoriPage.js
@@ -3,8 +3,11 @@ import TvMazeRequester from "../api/TvMazeRequester";
 import Card from "../Card";
 import PrototypeCard from "../PrototypeCard";
 import { favIsEmpty } from "../FavoriteHandler";
+import {Sorter} from "../Sorter";
 
 export class FavoriPage extends Page{
+
+    result = [];
 
     render() {
         return "<span class='wait'>Wait a moment plz </span>";
@@ -23,24 +26,51 @@ export class FavoriPage extends Page{
         const promiseArray = [];
 
         if (myFav.length > 0) {
+            const favToFetch = [];
 
-            const arrayElement = [];
-            myFav.forEach(id => promiseArray.push(request.getById(id).then(response => response.json())));
+            // this is for fetch only new favs
+            myFav.forEach( id => {
+                // only if result does not contain this id
+                if(!this.result.some( item => item.id === id)) favToFetch.push(id);
+            });
+
+            // this is to remove deleted favs
+            this.result.forEach( (item, index) => {
+                // only if favs IDs does not contain one item, so we remove him
+                if(!myFav.some( id => id === item.id)) {
+                    this.result.splice(index, 1); // remove deleted fav
+                }
+            });
+
+            favToFetch.forEach(id => 
+                promiseArray.push(request.getById(id).then(response => response.json()))
+            );
             
             Promise.allSettled(promiseArray).then((values) => {
                 values.forEach(item => {
-                    arrayElement.push(new PrototypeCard(item.value).render());    
+                    this.result.push(item.value);
                 })
             }).then(() => {
+                let data = Sorter.sort_array(this.result); // sort data
                 element.innerHTML = "";
-                arrayElement.forEach(item => element.appendChild(item));
+                data.forEach(item => {element.appendChild(new PrototypeCard(item).render())}) // show data
+
                 element.querySelector(".wait")?.classList.remove("wait");
             })
         } else {
             element.innerHTML = "<h3>Pas de favoris ...</h3>"
         }
-        
-        
+    }
+
+    redisplay(element) {
+        super.redisplay();
+
+        this.result = Sorter.sort_array(this.result);
+        element.innerHTML = "";
+
+        this.result.forEach( (item) => {
+            element.appendChild(new PrototypeCard(item).render());
+        })
 
     }
 

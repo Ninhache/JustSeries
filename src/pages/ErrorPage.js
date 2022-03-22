6e8d8c74e6e6f0ae541e59e4959ce11770dba8af
Eliott Collin <eliott.collin.etu@univ-lille.fr>
2022-03-22 10:55:36 +0100

    upgrade 404 page

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,9 +1,23 @@
 import { Page } from "./Page";
+import {Router} from "../Router";
 
 class ErrorPage extends Page {
 
+    mount(element) {
+        element.querySelector(".toHome").addEventListener("click", event => {
+            event.preventDefault();
+            Router.navigate("/");
+        })
+
+    }
+
     render() {
-        return "404 mon reuf";
+        return /*html*/ `
+            <div class="error_container">
+                <i class="fa-solid fa-user-secret" style="font-size: 10em"></i>
+                <div>Nothing to see here...</div>
+                <div>Please go to <div class="link_box"><a class="toHome">home page</a></div></div>            
+            </div>`;
     }
 
 }
